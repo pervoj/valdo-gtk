761d0d37b02c1931aae119a9aa431dd3f163650b
Vojtěch Perník <pervoj@gmx.com>
2021-08-08 13:45:07 +0200

    Update generate-pot.sh

--- generate-pot.sh
+++ generate-pot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 xgettext -f po/POTFILES -o po/valdo-gtk.pot -cTranslators: --from-code=UTF-8
+sed -i -e "s/Project-Id-Version: PACKAGE VERSION/Project-Id-Version: valdo-gtk/" po/valdo-gtk.pot
 msgmerge -U po/*.po po/valdo-gtk.pot
